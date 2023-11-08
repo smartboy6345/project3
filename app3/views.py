5c269a9f8ee4289bfd49fa017aaf913cc97023e5
smartboy6345 <duddujeevankumar@gmail.com>
2023-11-08 23:22:24 +0530

    first commit

---
+++ app3/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def silk(request):
+    return HttpResponse('<h1><marquee>We are not Talking about the dairymilk Slik only legends can understand it minimum graduation')
